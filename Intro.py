f81da31d91ee04c5d5d606373c1b7b418ddc416d
Viskytrn <gytongavisky@gmail.com>
2025-02-03 13:18:47 +0300

    Initial commit

---
+++ Intro.py
@@ -0,0 +1,6 @@
+#Print is a method used to display a value
+
+print("Glory")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ Variable.py
@@ -0,0 +1,24 @@
+#Variable is a container used to store a value
+
+number=10
+firstname="Vince"
+lastname="Gitonga"
+language="Python"
+a=10
+b=15
+c=25
+
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language,"is a web programming language")
+print(a*b)
+#The sum of 10 and 15 is 25
+print("The sum of", a, "and", b ,"is", c)
+
+
+
+
+
