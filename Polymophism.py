0300de50d9993b411ea644fb9d7ee8487e88827e
Viskytrn <bitokenock38@gmail.com>
2025-02-10 13:17:36 +0300

    Commit

---
+++ Polymophism.py
@@ -0,0 +1,20 @@
+class shape:
+    def draw(self):
+        print("Drawing a shape")
+
+class Rectangle:
+    def draw(self):
+        print("Drawing a rectangle")
+
+class Square:
+    def draw(self):
+        print("Drawing a square")
+
+sh=shape()
+sh.draw()
+
+
+
+
+
+S=Square
